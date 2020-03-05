a4711417a8af8f041f5b6dd3adb812308717268c
Khairil Yusof <khairil.yusof@gmail.com>
2020-03-05 22:13:16 +0800

    Fix indention

--- src/politikus/contenttypes/content/issue.py
+++ src/politikus/contenttypes/content/issue.py
@@ -15,8 +15,8 @@ class IIssue(model.Schema):
     """ Marker interface for Issue
     """
 
-     dexteritytextindexer.searchable('whatever_categories')
-     whatever_categories =  schema.Choice(
+    dexteritytextindexer.searchable('whatever_categories')
+    whatever_categories =  schema.Choice(
         title=_(u'Whatever Categories'),
         vocabulary=SimpleVocabulary.fromValues([_(u'foo'),_(u'bar'),_(u'far'),_(u'boo')]),
         required=False,
