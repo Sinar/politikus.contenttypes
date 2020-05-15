a155a0429e30bc951b76d9c2623df75481d276f4
Khairil Yusof <khairil.yusof@gmail.com>
2020-05-15 13:20:59 +0800

    "Add behavior: SourceType"

---
+++ src/politikus/contenttypes/tests/test_behavior_source_type.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from politikus.contenttypes.behaviors.source_type import ISourceTypeMarker
+from politikus.contenttypes.testing import POLITIKUS_CONTENTTYPES_INTEGRATION_TESTING  # noqa
+from plone.app.testing import setRoles
+from plone.app.testing import TEST_USER_ID
+from plone.behavior.interfaces import IBehavior
+from zope.component import getUtility
+
+import unittest
+
+
+class SourceTypeIntegrationTest(unittest.TestCase):
+
+    layer = POLITIKUS_CONTENTTYPES_INTEGRATION_TESTING
+
+    def setUp(self):
+        """Custom shared utility setup for tests."""
+        self.portal = self.layer['portal']
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+
+    def test_behavior_source_type(self):
+        behavior = getUtility(IBehavior, 'politikus.contenttypes.source_type')
+        self.assertEqual(
+            behavior.marker,
+            ISourceTypeMarker,
+        )
