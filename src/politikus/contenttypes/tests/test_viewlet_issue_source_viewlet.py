2c99cf45543b58d0f0821cd503bfa5edf9e6a064
Khairil Yusof <khairil.yusof@gmail.com>
2020-05-25 14:21:58 +0800

    "Add viewlet: issue-source-viewlet"

---
+++ src/politikus/contenttypes/tests/test_viewlet_issue_source_viewlet.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+from politikus.contenttypes.interfaces import IPolitikusContenttypesLayer
+from politikus.contenttypes.testing import POLITIKUS_CONTENTTYPES_FUNCTIONAL_TESTING
+from politikus.contenttypes.testing import POLITIKUS_CONTENTTYPES_INTEGRATION_TESTING
+from plone import api
+from plone.app.testing import setRoles
+from plone.app.testing import TEST_USER_ID
+from Products.Five.browser import BrowserView
+from zope.component import queryMultiAdapter
+from zope.interface import alsoProvides
+from zope.viewlet.interfaces import IViewletManager
+
+import unittest
+
+
+class ViewletIntegrationTest(unittest.TestCase):
+
+    layer = POLITIKUS_CONTENTTYPES_INTEGRATION_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+        self.app = self.layer['app']
+        self.request = self.app.REQUEST
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+        api.content.create(self.portal, 'Document', 'other-document')
+        api.content.create(self.portal, 'News Item', 'newsitem')
+
+    def test_issue_source_viewlet_is_registered(self):
+        view = BrowserView(self.portal['other-document'], self.request)
+        manager_name = 'plone.abovecontenttitle'
+        alsoProvides(self.request, IPolitikusContenttypesLayer)
+        manager = queryMultiAdapter(
+            (self.portal['other-document'], self.request, view),
+            IViewletManager,
+            manager_name,
+            default=None
+        )
+        self.assertIsNotNone(manager)
+        manager.update()
+        my_viewlet = [v for v in manager.viewlets if v.__name__ == 'issue-source-viewlet']  # NOQA: E501
+        self.assertEqual(len(my_viewlet), 1)
+
+    # XXX would be nice to have this test working:
+    # def test_issue_source_viewlet_is_not_available_on_newsitem(self):
+    #     view = BrowserView(self.portal['newsitem'], self.request)
+    #     manager_name = 'plone.abovecontenttitle'
+    #     alsoProvides(self.request, IPolitikusContenttypesLayer)
+    #     manager = queryMultiAdapter(
+    #         (self.portal['newsitem'], self.request, view),
+    #         IViewletManager,
+    #         manager_name,
+    #         default=None
+    #     )
+    #     self.assertIsNotNone(manager)
+    #     manager.update()
+    #     my_viewlet = [v for v in manager.viewlets if v.__name__ == 'issue-source-viewlet']  # NOQA: E501
+    #     self.assertEqual(len(my_viewlet), 0)
+
+
+class ViewletFunctionalTest(unittest.TestCase):
+
+    layer = POLITIKUS_CONTENTTYPES_FUNCTIONAL_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
